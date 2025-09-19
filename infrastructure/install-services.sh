f92407580821bf5a55ce02b82277f2aaa9c6061a
Shadi Mahmoodian <shadi.mahm@gmail.com>
2025-09-19 05:23:56 +0330

    Update install-services bash to support strimzi

--- infrastructure/install-services.sh
+++ infrastructure/install-services.sh
@@ -8,10 +8,17 @@ kubectl apply -f manifests/namespace.yaml
 
 echo "Adding Helm repositories..."
 helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add strimzi https://strimzi.io/charts/
 helm repo update
 
 echo "Installing ClickHouse single-node..."
 helm upgrade --install clickhouse bitnami/clickhouse --namespace data -f helm-values/clickhouse-values.yaml
 
-echo "Installing Kafka single-node..."
-helm upgrade --install kafka bitnami/kafka --namespace kafka -f helm-values/kafka-values.yaml
+echo "Installing Kafka operator..."
+helm upgrade --install strimzi-kafka-operator strimzi/strimzi-kafka-operator -n kafka
+
+echo "Creating Kafka..."
+kubectl apply -f manifests/kafka-cluster.yaml
+
+echo "Creating Kafka events topic..."
+kubectl apply -f manifests/events-topic.yaml
