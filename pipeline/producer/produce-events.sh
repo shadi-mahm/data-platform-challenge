db12d85cede6fbc84f1ad723d7fb6d0f49e7fa90
Shadi Mahmoodian <shadi.mahm@gmail.com>
2025-09-19 05:00:45 +0330

    Add bash script to produce events to kafka

---
+++ pipeline/producer/produce-events.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+echo "Starting to produce 100 records into Kafka topic 'events'..."
+
+kubectl run -n kafka kafka-producer -ti --rm --restart=Never \
+  --image=bitnami/kafka:latest -- \
+  bash -c '
+for i in $(seq 1 100); do
+  echo "{\"user_id\":\"user_$i\", \
+         \"event_type\":\"$(shuf -e login logout purchase view click -n 1)\", \
+         \"timestamp\":\"$(date -u +"%Y-%m-%dT%H:%M:%SZ")\", \
+         \"session_id\":\"sess_$((RANDOM%1000))\", \
+         \"device\":\"$(shuf -e mobile desktop tablet -n 1)\"}"
+done \
+| b bin/kafka-console-producer.sh \
+    --bootstrap-server kafka-cluster-kafka-bootstrap:9092 \
+    --topic events \
+    --producer-property acks=all \
+    --producer-property enable.idempotence=true
+'
+
+echo "Done!"
